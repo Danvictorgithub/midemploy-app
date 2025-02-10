0eb546cb6903e1a768d7ff1e606d44b7fd612a87
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-10 20:08:01 +0800

    adonisjs initialized

---
+++ backend/start/routes.ts
@@ -0,0 +1,16 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.get('/', async () => {
+  return {
+    message: 'Midemply Api V1.0.0',
+  }
+})
